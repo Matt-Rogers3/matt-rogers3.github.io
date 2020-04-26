1998321d9580d34719d88d4040ad701ee47daf3e
Matt-Rogers3 <mrogers3@byui.edu>
2020-04-25 23:34:59 -0600

    update home page

--- js/currentdate.js
+++ js/currentdate.js
@@ -1,2 +1,2 @@
 const options = {weekday: 'long', day: 'numeric', month: 'long', year:'numeric'};
-document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US', options);
+document.getElementById('currentdate').textContent = new Date(document.lastModified).toLocaleDateString('en-US', options);
